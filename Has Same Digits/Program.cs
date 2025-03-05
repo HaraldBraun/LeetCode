d42c4e0adaf512d1a1b8d3262dab213dd6a134c5
OldJuck <Harald.L.Braun@outlook.de>
2025-03-05 19:19:36 +0100

    Some more stuff

---
+++ Has Same Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Has_Same_Digits {
+    internal class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "Hello, World!" );
+            Console.WriteLine( HasSameDigits( "3902" ) );
+            Console.WriteLine( HasSameDigits( "34789" ) );
+            Console.ReadLine( );
+        }
+
+        public static bool HasSameDigits( string s ) {
+            int[] numbers = s.Select(c => c -'0').ToArray();
+            while (numbers.Length > 2) {
+                numbers = AddPairs( numbers );
+            }
+            return numbers[0] == numbers[1];
+        }
+
+        private static int[] AddPairs( int[] numbers) {
+            int[] result = new int[numbers.Length - 1];
+            for (int i = 0; i < numbers.Length - 1; i++) {
+                result[i] = (numbers[i] + numbers[i + 1]) % 10;
+            }
+            return result;
+        }
+    }
+}
---
+++ IsSameTree/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IsSameTree {
+    internal class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "Hello, World!" );
+        }
+
+        public bool IsSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            if (p.val != q.val) {
+                return false;
+            }
+            return IsSameTree( p.left, q.left ) && IsSameTree( p.right, q.right );
+        }
+    }
+
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode( int val, TreeNode left, TreeNode right ) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
